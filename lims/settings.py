9115245fcce80c63d52da4ff2976a20460549161
Administrator <administrator@example.com>
2025-10-03 09:40:36 +0800

    收集静态资源

--- lims/settings.py
+++ lims/settings.py
@@ -14,9 +14,9 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'git_lims_static'
 
 
 # Quick-start development settings - unsuitable for production
