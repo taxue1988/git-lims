48d5e5afbec7483288eced8e68ad576a40cf357b
taxue1988 <yuanzhen@iccas.ac.cn>
2025-10-14 15:20:12 +0800

    管理员增加测试页面

--- lims/settings.py
+++ lims/settings.py
@@ -99,7 +99,7 @@ else:
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'lims',
             'USER': 'root',
-            'PASSWORD': 'root123456',
+            'PASSWORD': '',
             'HOST': '127.0.0.1',
             'PORT': '3306',
         }
