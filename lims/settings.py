6150b8aed00d965d54da647f6332e0a2d32e9016
taxue1988 <yuanzhen@iccas.ac.cn>
2025-10-20 10:28:41 +0800

    20251020-宋世雄-02-重新上传（github与本地文件不一致）

--- lims/settings.py
+++ lims/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_ROOT = BASE_DIR / 'git_lims_static'
 
